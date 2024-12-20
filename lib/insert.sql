b481cdd22f2d0e609678879c66b86707da84364b
MBURU <wawerumburu157@gmail.com>
2024-12-20 09:50:42 +0300

    CHANGES

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'brown', 'friendly', 1),
+('Rowdy', 3, 'M', 'black', 'aggressive', 1),
+('Tabitha', 2, 'F', 'golden', 'playful', 1),
+('Sergeant Brown', 4, 'M', 'brown', 'calm', 1),
+('Melissa', 1, 'F', 'white', 'curious', 1),
+('Grinch', 6, 'M', 'green', 'grumpy', 1),
+('Wendy', 2, 'F', 'black', 'gentle', 1),
+(NULL, 1, 'M', 'unknown', 'shy', 1); 
